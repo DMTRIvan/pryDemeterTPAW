32b269c16e64e816f1e347750a31a8daed465ab7
Ivan <navi.dmtr@gmail.com>
2023-12-14 23:36:14 -0300

    Organice los directorios

--- Recursos/js/producto.js
+++ Recursos/js/producto.js
@@ -4,37 +4,37 @@ const productos = {
         {
             numero: 1,
             nombre: "En el camino, de Jack Kerouac. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/OnTheRoad.JPG"
+            imagen: "./recursos/fotoslibros/ontheroad.JPG"
         },
         {
             numero: 2,
             nombre: "¡Harry Potter! Los siete libros en tapa dura, en excelente estado. Precio: $15000",
-            imagen: "./Recursos/fotosLibros/HarryPotter.JPG"
+            imagen: "./recursos/fotoslibros/harrypotter.JPG"
         },
         {
             numero: 3,
             nombre: "Big Sur, de Jack Kerouac. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/BigSurKerouac.jpg"
+            imagen: "./recursos/fotoslibros/bigsurkerouac.jpg"
         },
         {
             numero: 4,
             nombre: "Cuentos selectos, de Hermann Hesse. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/HERMANN_HESSE.jpg"
+            imagen: "./recursos/fotoslibros/herman-hesse.jpg"
         },
         {
             numero: 5,
             nombre: "La Odisea, de Homero. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/HomeroLaOdisea.jpg"
+            imagen: "./recursos/fotoslibros/homerolaodisea.jpg"
         },
         {
             numero: 6,
             nombre: "Julio Verne. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/Julio_Verne.jpg"
+            imagen: "./recursos/fotoslibros/julio_verne.jpg"
         },
         {
             numero: 7,
             nombre: "Vagabundos del Dharma, de Jack Kerouac. Tapa blanda, como nuevo. Precio: $5000",
-            imagen: "./Recursos/fotosLibros/VagabundosDharmaKerouac.jpg"
+            imagen: "./recursos/fotoslibros/vagabundosdharmakerouac.jpg"
         }
 
     ]
